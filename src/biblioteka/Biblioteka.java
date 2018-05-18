943d4320f5f7c8b5f9044a7888aa891b4c7133d2
JovanaDrobnjakovic <drobnjakovic2216@fon.bg.ac.rs>
2018-05-18 18:50:34 +0200

    izmenjena putanaj

---
+++ src/biblioteka/Biblioteka.java
@@ -0,0 +1,46 @@
+package biblioteka;
+
+import java.util.LinkedList;
+
+import biblioteka.interfejs.BibliotekaInterfejs;
+
+public class Biblioteka implements BibliotekaInterfejs {
+
+	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
+	
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		if (knjiga == null || knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga je null ili vec postoji");
+		
+		knjige.add(knjiga);
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		if (knjiga == null || !knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga je null ili ne postoji");
+
+		knjige.remove(knjiga);
+	}
+
+	@Override
+	public LinkedList<Knjiga> vratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, String isbn, String izdavac) {
+		if (naslov == null)
+			throw new RuntimeException("Morate uneti naslov");
+		
+		LinkedList<Knjiga> novaLista = new LinkedList<Knjiga>();
+		
+		for(int i=0;i<knjige.size();i++)
+			if (knjige.get(i).getNaslov().contains(naslov))
+				novaLista.add(knjige.get(i));
+		
+		return novaLista;
+	}
+
+}
